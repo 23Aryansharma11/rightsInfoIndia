45c1961ab2abb0da15f426aed1794a906e04b7b3
Aryan Sharma <23aryansharma11@gmail.com>
2024-08-10 16:16:13 +0530

    add home contribute blog page

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,53 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'RightsInfoIndia';
+export const SITE_DESCRIPTION = 'RightsInfoIndia is a blog site dedicated to empowering individuals with comprehensive knowledge about their legal rights in India. The site aims to provide accessible and accurate information on various legal topics, helping users navigate their rights and responsibilities effectively.';
+
+
+export const contributors = [
+    {
+        name: "John Doe",
+        image: "https://img.freepik.com/free-photo/medium-shot-man-working-as-lawyer_23-2151054043.jpg?t=st=1723281637~exp=1723285237~hmac=b435d18848d11f912834f695eb59174590e2a7a71bed93933b692cf353bcf425&w=826",
+    },
+    {
+        name: "John Doe",
+        image: "https://img.freepik.com/free-photo/medium-shot-man-working-as-lawyer_23-2151054043.jpg?t=st=1723281637~exp=1723285237~hmac=b435d18848d11f912834f695eb59174590e2a7a71bed93933b692cf353bcf425&w=826",
+    },
+    {
+        name: "John Doe",
+        image: "https://img.freepik.com/free-photo/medium-shot-man-working-as-lawyer_23-2151054043.jpg?t=st=1723281637~exp=1723285237~hmac=b435d18848d11f912834f695eb59174590e2a7a71bed93933b692cf353bcf425&w=826",
+    },
+    {
+        name: "John Doe",
+        image: "https://img.freepik.com/free-photo/medium-shot-man-working-as-lawyer_23-2151054043.jpg?t=st=1723281637~exp=1723285237~hmac=b435d18848d11f912834f695eb59174590e2a7a71bed93933b692cf353bcf425&w=826",
+    },
+    {
+        name: "John Doe",
+        image: "https://img.freepik.com/free-photo/medium-shot-man-working-as-lawyer_23-2151054043.jpg?t=st=1723281637~exp=1723285237~hmac=b435d18848d11f912834f695eb59174590e2a7a71bed93933b692cf353bcf425&w=826",
+    },
+    {
+        name: "John Doe",
+        image: "https://img.freepik.com/free-photo/medium-shot-man-working-as-lawyer_23-2151054043.jpg?t=st=1723281637~exp=1723285237~hmac=b435d18848d11f912834f695eb59174590e2a7a71bed93933b692cf353bcf425&w=826",
+    },
+    {
+        name: "John Doe",
+        image: "https://img.freepik.com/free-photo/medium-shot-man-working-as-lawyer_23-2151054043.jpg?t=st=1723281637~exp=1723285237~hmac=b435d18848d11f912834f695eb59174590e2a7a71bed93933b692cf353bcf425&w=826",
+    },
+    {
+        name: "John Doe",
+        image: "https://img.freepik.com/free-photo/medium-shot-man-working-as-lawyer_23-2151054043.jpg?t=st=1723281637~exp=1723285237~hmac=b435d18848d11f912834f695eb59174590e2a7a71bed93933b692cf353bcf425&w=826",
+    },
+    {
+        name: "John Doe",
+        image: "https://img.freepik.com/free-photo/medium-shot-man-working-as-lawyer_23-2151054043.jpg?t=st=1723281637~exp=1723285237~hmac=b435d18848d11f912834f695eb59174590e2a7a71bed93933b692cf353bcf425&w=826",
+    },
+    {
+        name: "John Doe",
+        image: "https://img.freepik.com/free-photo/medium-shot-man-working-as-lawyer_23-2151054043.jpg?t=st=1723281637~exp=1723285237~hmac=b435d18848d11f912834f695eb59174590e2a7a71bed93933b692cf353bcf425&w=826",
+    },
+    {
+        name: "John Doe",
+        image: "https://img.freepik.com/free-photo/medium-shot-man-working-as-lawyer_23-2151054043.jpg?t=st=1723281637~exp=1723285237~hmac=b435d18848d11f912834f695eb59174590e2a7a71bed93933b692cf353bcf425&w=826",
+    },
+]
