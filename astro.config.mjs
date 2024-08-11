c18d3a009c0aeadc03c9ccb2fb335bda3444f6ae
Aryan Sharma <23aryansharma11@gmail.com>
2024-08-11 20:29:20 +0530

    add search bar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-	image:{
-		domains: ["img.freepik.com"]
-	}
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), react()],
+  image: {
+    domains: ["img.freepik.com"]
+  }
 });
--- src/consts.ts
+++ src/consts.ts
@@ -51,3 +51,32 @@ export const contributors = [
         image: "https://img.freepik.com/free-photo/medium-shot-man-working-as-lawyer_23-2151054043.jpg?t=st=1723281637~exp=1723285237~hmac=b435d18848d11f912834f695eb59174590e2a7a71bed93933b692cf353bcf425&w=826",
     },
 ]
+
+interface searchData {
+    title: string;
+    slug: string;
+}
+
+export const search: searchData[] = [
+    {
+        title: "How to File a Complaint in Consumer Court",
+        slug: "how-to-file-a-complaint-in-consumer-court"
+    },
+    {
+        title: "Is It Legal to Film a Police Officer in India?",
+        slug: "is-it-legal-to-film-a-police-officer-in-india"
+    },
+    {
+        title: "Process of Filing an FIR",
+        slug: "process-of-filing-an-FIR"
+    },
+    {
+        title: "Start Exploring",
+        slug: "start-exploring"
+    },
+    {
+        title: "Understanding Your Rights During Police Interrogation in India",
+        slug: "understanding-your-rights-during-police-interrogation-in-india"
+    },
+
+]
